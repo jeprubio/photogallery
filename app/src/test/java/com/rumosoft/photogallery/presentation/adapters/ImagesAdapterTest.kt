951822773801cfd62461412fbe9fc0beeb6b8304
jeprubio <jeprubio@gmail.com>
2021-06-02 22:37:54 +0200

    [Add] A couple of unit tests

---
+++ app/src/test/java/com/rumosoft/photogallery/presentation/adapters/ImagesAdapterTest.kt
@@ -0,0 +1,54 @@
+package com.rumosoft.photogallery.presentation.adapters
+
+import com.rumosoft.photogallery.Samples
+import com.rumosoft.photogallery.data.network.mappers.toImage
+import com.rumosoft.photogallery.presentation.listeners.ImageClickListener
+import io.mockk.every
+import io.mockk.spyk
+import org.junit.Assert
+import org.junit.jupiter.api.Test
+
+internal class ImagesAdapterTest {
+    private var sut: ImagesAdapter =
+            spyk(ImagesAdapter(ImageClickListener { itemId ->
+                // Do nothing
+            }, ImageClickListener {
+                // Do nothing
+            }))
+
+    init {
+        every { sut.notifyDataSetChanged() } answers { nothing }
+    }
+
+    @Test
+    fun `The adapter data can be updated`() {
+        // Arrange
+        sut.data = listOf()
+        Assert.assertEquals(0, sut.data.size)
+
+        // Act
+        sut.data = listOf(
+                Samples.sampleApiImage().toImage(), Samples.sampleApiImage().toImage().copy(id = 2L)
+        )
+
+        // Assert
+        Assert.assertEquals(2, sut.data.size)
+    }
+
+    @Test
+    fun `itemCount() returns the number of saved data`() {
+        // Act
+        sut.data = listOf(
+                Samples.sampleApiImage().toImage(), Samples.sampleApiImage().toImage().copy(id = 2L)
+        )
+
+        // Assert
+        Assert.assertEquals(2, sut.itemCount)
+    }
+
+    @Test
+    fun `If no data is assigned returns zero`() {
+        // Assert
+        Assert.assertEquals(0, sut.itemCount)
+    }
+}
---
+++ app/src/test/java/com/rumosoft/photogallery/presentation/details/DetailsViewModelTest.kt
@@ -0,0 +1,102 @@
+package com.rumosoft.photogallery.presentation.details
+
+import android.text.Editable
+import androidx.lifecycle.SavedStateHandle
+import com.google.gson.Gson
+import com.rumosoft.photogallery.InstantExecutorExtension
+import com.rumosoft.photogallery.MainCoroutineRule
+import com.rumosoft.photogallery.Samples
+import com.rumosoft.photogallery.data.network.mappers.toImage
+import com.rumosoft.photogallery.domain.usecases.interfaces.AddImageUseCase
+import com.rumosoft.photogallery.domain.usecases.interfaces.UpdateImageTitleUseCase
+import com.rumosoft.photogallery.infrastructure.Resource
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.MockK
+import io.mockk.mockk
+import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert.assertNotEquals
+import org.junit.Rule
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+
+@ExperimentalCoroutinesApi
+@ExtendWith(InstantExecutorExtension::class)
+internal class DetailsViewModelTest {
+    @get:Rule
+    val coroutineRule = MainCoroutineRule(TestCoroutineDispatcher())
+
+    @MockK
+    lateinit var addImageUseCase: AddImageUseCase
+
+    @MockK
+    lateinit var updateImageTitleUseCase: UpdateImageTitleUseCase
+
+    private var sampleImage = Samples.sampleApiImage().toImage()
+
+    init {
+        MockKAnnotations.init(this)
+        coEvery {
+            addImageUseCase(any())
+        } returns Resource.Success(1L)
+        coEvery {
+            updateImageTitleUseCase(any())
+        } returns Resource.Success(Samples.sampleApiImage().toImage())
+    }
+
+    @Test
+    fun `saveImage() calls UpdateImageTitle use case if it has id greater than 0`() =
+            coroutineRule.testDispatcher.runBlockingTest {
+                // Arrange
+                val savedStateHandle = SavedStateHandle().apply {
+                    set("item", Gson().toJson(sampleImage))
+                }
+                val sut = DetailsViewModel(addImageUseCase, updateImageTitleUseCase, savedStateHandle)
+
+                // Act
+                sut.saveImage()
+
+                // Assert
+                coVerify { updateImageTitleUseCase(any()) }
+            }
+
+    @Test
+    fun `saveImage() calls addImageUseCase use case if it has id lower or equal to 0`() =
+            coroutineRule.testDispatcher.runBlockingTest {
+                // Arrange
+                val savedStateHandle = SavedStateHandle().apply {
+                    set("item", Gson().toJson(sampleImage.copy(id = -1L)))
+                }
+                val sut = DetailsViewModel(addImageUseCase, updateImageTitleUseCase, savedStateHandle)
+
+                // Act
+                sut.saveImage()
+
+                // Assert
+                coVerify { addImageUseCase(any()) }
+            }
+
+    @Test
+    fun `afterTitleChanged() updates the image LiveData value`() =
+            coroutineRule.testDispatcher.runBlockingTest {
+                // Arrange
+                val savedStateHandle = SavedStateHandle().apply {
+                    set("item", Gson().toJson(sampleImage))
+                }
+                val sut = DetailsViewModel(addImageUseCase, updateImageTitleUseCase, savedStateHandle)
+                val editable = mockk<Editable>()
+                val editableString = "PhotoGallery Rules!"
+                coEvery { editable.toString() } returns editableString
+                assertNotEquals(editableString, sut.image.value!!.title)
+
+                // Act
+                sut.afterTitleChanged(editable)
+
+                // Assert
+                assertEquals(editableString, sut.image.value!!.title)
+            }
+}
